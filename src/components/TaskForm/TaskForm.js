e43d3e57cc89693a91ee93e926903778b182fa81
dmitrmedv <dmitrmedv@gmail.com>
2023-10-13 09:08:22 +0300

    add

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,10 +1,15 @@
+import { useDispatch } from 'react-redux';
 import { Button } from 'components/Button/Button';
+import { addTask } from '../../redux/actions';
 import css from './TaskForm.module.css';
 
 export const TaskForm = () => {
+  const dispatch = useDispatch();
+
   const handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
+    dispatch(addTask(form.elements.text.value, form.elements.phone.value));
     form.reset();
   };
 
@@ -14,6 +19,12 @@ export const TaskForm = () => {
         className={css.field}
         type="text"
         name="text"
+        placeholder="Enter task text..."
+      />
+      <input
+        className={css.field}
+        type="text"
+        name="phone"
         placeholder="Enter task text..."
       />
       <Button type="submit">Add task</Button>
---
+++ src/redux/reducer.js
@@ -0,0 +1,43 @@
+import { combineReducers } from 'redux';
+import { statusFilters } from './constants';
+
+const tasksInitialState = [];
+
+const tasksReducer = (state = tasksInitialState, action) => {
+  switch (action.type) {
+    case 'tasks/addTask':
+      return [...state, action.payload];
+    case 'tasks/deleteTask':
+      return state.filter(task => task.id !== action.payload);
+    case 'tasks/toggleCompleted':
+      return state.map(task => {
+        if (task.id !== action.payload) {
+          return task;
+        }
+        return { ...task, completed: !task.completed };
+      });
+    default:
+      return state;
+  }
+};
+
+const filtersInitialState = {
+  status: statusFilters.all,
+};
+
+const filtersReducer = (state = filtersInitialState, action) => {
+  switch (action.type) {
+    case 'filters/setStatusFilter':
+      return {
+        ...state,
+        status: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  filters: filtersReducer,
+});
